84b810a0998ca2ad31460972523161411f07573e
geekbert <philsophia@gmail.com>
2014-09-03 05:33:32 +0000

    3rd commit after finishing chapter 5

--- code/tango_with_django_project/rango/admin.py
+++ code/tango_with_django_project/rango/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category')
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
+# To customise the admin interface, you will need to edit rango/admin.py and create a PageAdmin class that inherits from admin.ModelAdmin
+
+#class PageAdmin(admin.ModelAdmin):    
+#    list_display = ('title', 'category')
+
+#admin.site.register(PageAdmin)
--- code/tango_with_django_project/tango_with_django_project/urls.py
+++ code/tango_with_django_project/tango_with_django_project/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
-
+    url(r'^admin/', include(admin.site.urls)), # ADD THIS LINE
 )
 
 
